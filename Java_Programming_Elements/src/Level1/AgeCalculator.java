fab183970c0d9f572d4754f3475daf5a664c721b
Prerit Jain <preritjain@gmail.com>
2025-06-12 10:05:01 +0530

    Level1 AgeCalculator Added

---
+++ Java_Programming_Elements/src/Level1/AgeCalculator.java
@@ -0,0 +1,10 @@
+package Level1;
+
+public class AgeCalculator {
+
+	public static void main(String[] args) {
+		int currentYear = 2024;
+		System.out.println("Harry's age in 2024 is " + (currentYear-2000));
+	}
+
+}
---
+++ Java_Programming_Elements/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Java_Programming_Elements {
+}
