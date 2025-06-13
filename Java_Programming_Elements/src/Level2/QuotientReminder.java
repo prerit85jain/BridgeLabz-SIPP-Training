c9fe81d37a695137022b235c9dcb8f28e932a8de
Prerit Jain <preritjain@gmail.com>
2025-06-13 08:46:44 +0530

    Level2 add. Completed Level2 Assignment

---
+++ Java_Programming_Elements/src/Level2/QuotientReminder.java
@@ -0,0 +1,14 @@
+package Level2;
+import java.util.*;
+public class QuotientReminder {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
+		
+		System.out.println("The Quotient is "+(num1/num2)+" and Reminder is "+(num1%num2)+" of two number "+num1+" and "+num2);
+		sc.close();
+	}
+
+}
