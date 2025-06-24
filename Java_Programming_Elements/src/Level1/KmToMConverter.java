dfa826c97937de254b6a4e9ef65577ba0ddf12f3
Prerit Jain <preritjain@gmail.com>
2025-06-24 22:44:31 +0530

    Prerit add . constructors level01 completed

--- Java_Programming_Elements/src/Level1/KmToMConverter.java
+++
@@ -1,9 +0,0 @@
-package Level1;
-
-public class KmToMConverter {
-	public static void main(String args[]) {
-		double km = 10.8;
-		double mile = km*1.6;
-		System.out.println("The distance "+km+ " km in miles is "+mile);
-	}
-}
--- Java_Programming_Elements/src/module-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-/**
- * 
- */
-module Java_Programming_Elements {
-}
